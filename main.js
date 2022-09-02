cce7e210310627a7f2d68eed5127f07adb0cc0e9
xossiane <anejosi.lima@gmail.com>
2022-09-02 15:18:26 -0300

    Projeto finalizado

--- main.js
+++ main.js
@@ -1,23 +1,57 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+const canvas = document.querySelector('#draw')
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = '#BADA55'
+ctx.lineJoin = 'round'; //It is just name for variable. It could be anything. Ctx is just short word for ConTeXt.
+ctx.lineCap = 'round';
+ctx.lineWidth = 5;
+ctx.globalCompositeOperation = 'multiply'
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+let direction = true;
+
+function draw(e){
+  if(!isDrawing) return; //stop function from drawing 
+  console.log(e);
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+  
+  ctx.beginPath();
+  //start from
+  ctx.moveTo(lastX, lastY);
+  //go to
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke()
+  lastX = e.offsetX;
+  lastY = e.offsetY
+  hue++;
+  if(hue >= 360){
+    hue = 0;
+  }
+
+  if(ctx.lineWidth >= 100 ||ctx.lineWidth <= 1){
+    direction = !direction;
+
+  }
+ if(direction){
+  ctx.lineWidth++;
+ } else{
+  ctx.lineWidth--;
+ }
+}
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+
+} )
+
+canvas.addEventListener('mousemove', draw)
+canvas.addEventListener('mouseup',() => isDrawing = false)
+canvas.addEventListener('mouseout',() => isDrawing = false)
